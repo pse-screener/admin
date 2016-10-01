fc1a53ad76d934d1cc3c41d8e0532bae701f77ad
jhunexjun <jdream_catcher@yahoo.com>
2016-10-02 02:36:42 +0800

    Lots of changes.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -81,11 +81,48 @@ pseController.controller('humanResourceCtrl', ['$scope', '$window', '$http',
 	}
 ]);
 
-/*pseController.controller('alertCtrl', [
-	function() {
+pseController.controller('CompaniesCtrl', ['$scope', '$http',
+	function($scope, $http) {
+		$scope.companyName = "Select company above";
 
+		var absUrl = "http://www.pse-screener.com/company";
+		var config = {
+			headers: {
+				Accept: 'Application/json',
+				Authorization: 'Bearer '.concat(sessionStorage.getItem("access_token"))
+			}
+		};
+
+		var successCallback = function(response) {
+			$scope.companies = response.data;
+		}
+		var errorCallback = function(response) {
+			console.log("Error: Cannot retrieve companies.");
+		}
+
+		$http.get(absUrl, config).then(successCallback, errorCallback);
+
+		/**/
+		$scope.update = function() {
+			var absUrl = "http://www.pse-screener.com/company/".concat($scope.companySymbol);
+			var config = {
+				headers: {
+					Accept: 'Application/json',
+					Authorization: 'Bearer '.concat(sessionStorage.getItem("access_token"))
+				}
+			};
+
+			var successCallback = function(response) {
+				$scope.companyName = response.data[0].companyName;
+			}
+			var errorCallback = function(response) {
+				console.log("Error: Cannot retrieve companies.");
+			}
+			
+			$http.get(absUrl, config).then(successCallback, errorCallback);
+		}
 	}
-]);*/
+]);
 
 pseController.controller('welcomeCtrl', ['$scope', '$window',
 	function($scope, $window) {
